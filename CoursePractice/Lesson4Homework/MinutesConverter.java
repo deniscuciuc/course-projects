580141a9f8875fe600e72c1769a5c879ad5bb439
deniscuciuc <86614824+deniscuciuc@users.noreply.github.com>
2022-02-02 19:07:06 +0200

    New Update for third task

--- CoursePractice/Lesson4Homework/MinutesConverter.java
+++ CoursePractice/Lesson4Homework/MinutesConverter.java
@@ -3,16 +3,14 @@ package Lesson4Homework;
 public class MinutesConverter {
     public static void main(String[] args) {
         long Minutes = 5;
-        double Days = 365;
-        double Years = 1;
+        final double Days = 365;
+        final double Years = 1;
 
-        double MinutesInHour = 60;
-        double MinutesInDay = MinutesInHour * 24;
-        double MinutesInYear = MinutesInDay * Days;
-
-
-        double ConvertMinutestoDays = Minutes / MinutesInDay;
-        double ConvertMinutestoYears = Minutes / MinutesInYear;
+        double  MinutesInHour = 60,
+                MinutesInDay = MinutesInHour * 24,
+                MinutesInYear = MinutesInDay * Days,
+                ConvertMinutestoDays = Minutes / MinutesInDay,
+                ConvertMinutestoYears = Minutes / MinutesInYear;
 
         System.out.println(Minutes + " minutes in days is " + ConvertMinutestoDays);
         System.out.println(Minutes + " minutes in years is " + ConvertMinutestoYears);
