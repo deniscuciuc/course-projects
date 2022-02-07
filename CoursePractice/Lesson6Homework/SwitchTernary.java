500e296e3e43669189c1ad1ee93c84785ff450ba
deniscuciuc <86614824+deniscuciuc@users.noreply.github.com>
2022-02-07 14:38:59 +0200

    SwitchTernary task

--- CoursePractice/Lesson6Homework/SwitchTernary.java
+++ CoursePractice/Lesson6Homework/SwitchTernary.java
@@ -1,8 +1,18 @@
 package Lesson6Homework;
 
+import java.util.Objects;
+
 public class SwitchTernary {
     public static void main (String[] args) {
         int colorCode = 101;
-        String color = null;
+        String color = "Red";
+        colorCode = (Objects.equals (color, "Yellow"))
+                ? 100 : (Objects.equals (color, "Green"))
+                ? 101 : (Objects.equals (color, "Red"))
+                ? 102 : 0;
+
+        System.out.println("Color ---&gt;"+color);
+        System.out.println(colorCode);
+
     }
 }
