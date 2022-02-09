38030783d41268fe6c95f94822f37a91c9017a31
deniscuciuc <86614824+deniscuciuc@users.noreply.github.com>
2022-02-09 14:35:37 +0200

    Lesson7homework - task 1

---
+++ CoursePractice/Lesson7Homework/DisplayNumbers.java
@@ -0,0 +1,18 @@
+package Lesson7Homework;
+
+public class DisplayNumbers {
+    public static void main (String[] args) {
+
+        for (int i = 100, j = 0; i <= 1000; i++) {
+            if (i % 5 == 0 && i % 6 == 0) {
+                System.out.print(i + " ");
+                j++;
+            } else {
+                continue;
+            }
+            if (j % 10 == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
