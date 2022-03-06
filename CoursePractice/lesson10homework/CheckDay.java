8723311486d828860370021e30f5e97ab3e9835b
deniscuciuc <86614824+deniscuciuc@users.noreply.github.com>
2022-03-06 18:21:27 +0200

    Little update

--- CoursePractice/lesson10homework/CheckDay.java
+++ CoursePractice/lesson10homework/CheckDay.java
@@ -1,12 +1,39 @@
 package lesson10homework;
 
 public class CheckDay extends Main {
-    private enum daysOfWeek {
-        s
+    private enum Weekday {
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY;
+
+        boolean isWeekDay () {
+            boolean isWeekDay = true;
+            if (this == SATURDAY || this == SUNDAY) {
+                isWeekDay = false;
+            }
+            return isWeekDay;
+        }
+
+        boolean isHoliday () {
+            boolean isHolidayDay = true;
+            if (this != SATURDAY && this != SUNDAY) {
+                isHolidayDay = false;
+            }
+            return isHolidayDay;
+        }
 
     }
 
     public static void checkDay() {
-
+        System.out.println(Weekday.SUNDAY.isWeekDay());
+        System.out.println(Weekday.MONDAY.isWeekDay());
+        System.out.println(Weekday.THURSDAY.isHoliday());
+        System.out.println(Weekday.SUNDAY.isHoliday());
+        System.out.println(Weekday.SATURDAY.isHoliday());
+        System.out.println(Weekday.TUESDAY.isWeekDay());
     }
 }
