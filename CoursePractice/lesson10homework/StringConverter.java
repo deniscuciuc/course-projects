2098b1e20e2af4772aa7393127cb06b16d91b1f3
deniscuciuc <86614824+deniscuciuc@users.noreply.github.com>
2022-03-06 18:21:53 +0200

    Added construct and display for wrapper

--- CoursePractice/lesson10homework/StringConverter.java
+++ CoursePractice/lesson10homework/StringConverter.java
@@ -1,7 +1,22 @@
 package lesson10homework;
 
 public class StringConverter extends Main {
-    public static void stringConverter() {
 
+    public static void stringConverter() {
+        Boolean booleanWrapperFromString = new Boolean("tRue");
+        Byte byteWrapperFromString = new Byte("120");
+        Short shortWrapperFromString = new Short("32000");
+        Integer intWrapperFromString = new Integer("147895632");
+        Long longWrapperFromString = new Long("987654321987654321");
+        Double doubleWrapperFromString = new Double("4444.5d");
+        Float floatWrapperFromString = new Float("2354.56f");
+
+        System.out.println(booleanWrapperFromString);
+        System.out.println(byteWrapperFromString);
+        System.out.println(shortWrapperFromString);
+        System.out.println(intWrapperFromString);
+        System.out.println(longWrapperFromString);
+        System.out.println(doubleWrapperFromString);
+        System.out.println(floatWrapperFromString);
     }
 }
