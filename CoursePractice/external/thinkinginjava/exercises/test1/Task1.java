810214c4e5e13ee8fd476b8199245ee3ee432686
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-03-13 10:57:26 +0200

    First test from thinking in java

---
+++ CoursePractice/external/thinkinginjava/exercises/test1/Task1.java
@@ -0,0 +1,10 @@
+package external.thinkinginjava.exercises.test1;
+
+public class Task1 {
+    public static void taskMain() {
+        int n = 0;
+        char m = 'm';
+        System.out.println(n);
+        System.out.println(m);
+    }
+}
---
+++ CoursePractice/external/thinkinginjava/exercises/test1/Task2.java
@@ -0,0 +1,10 @@
+package external.thinkinginjava.exercises.test1;
+
+import java.util.Date;
+
+public class Task2 {
+    public static void taskMain() {
+        System.out.println("Hello world!");
+        System.out.println("Now is: " + new Date());
+    }
+}
