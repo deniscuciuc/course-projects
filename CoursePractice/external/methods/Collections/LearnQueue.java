99fb0f639d172c8cf65a377a8f7e649968665a43
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-03-26 06:53:47 +0200

    Practice

---
+++ CoursePractice/external/methods/Collections/LearnQueue.java
@@ -0,0 +1,16 @@
+package external.methods.Collections;
+
+import java.util.*;
+
+public class LearnQueue {
+    public static void main(String[] args) {
+        // LinkedList implementation of Queue
+        Queue<String> animal1 = new LinkedList<>();
+
+        // Array implementation of Queue
+        Queue<String> animal2 = new ArrayDeque<>();
+
+        // Priority Queue implementation of Queue
+        Queue<String> animal3 = new PriorityQueue<>();
+    }
+}
---
+++ CoursePractice/external/methods/Collections/Map.java
@@ -0,0 +1,7 @@
+package external.methods.Collections;
+
+public class Map {
+    public static void main(String[] args) {
+
+    }
+}
