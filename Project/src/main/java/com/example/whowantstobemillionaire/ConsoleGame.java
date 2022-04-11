0ef8048b2703a73383af2c5469ea7079b1e2b98a
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-04-11 20:30:33 +0300

    Added project

---
+++ Project/src/main/java/com/example/whowantstobemillionaire/ConsoleGame.java
@@ -0,0 +1,10 @@
+package com.example.whowantstobemillionaire;
+
+public class ConsoleGame {
+    public void launchGame() {
+        /* TODO: here write a while loop (all game before user will stop it or will win or lose the game)
+         *  then write nested switch and take from user every value
+         */
+
+    }
+}
---
+++ Project/src/main/java/com/example/whowantstobemillionaire/WhoWantsToBeMillionaireApp.java
@@ -0,0 +1,11 @@
+package com.example.whowantstobemillionaire;
+
+import java.util.LinkedList;
+
+public class WhoWantsToBeMillionaireApp {
+    public static void main(String[] args) {
+        ConsoleGame consoleGame = new ConsoleGame();
+        consoleGame.launchGame();
+
+    }
+}
